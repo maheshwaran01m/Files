924af5a0d02f8fd1fe5324f53b35ddb5968c5de2
MAHESHWARAN <maheshwaran@eam360.com>
2023-06-04 08:14:59 +0530

    Initial Setup

---
+++ Files/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Files
+//
+//  Created by MAHESHWARAN on 04/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureView()
+  }
+  
+  private func configureView() {
+    self.title = "Files"
+    self.view.backgroundColor = .systemBackground
+  }
+}
+
